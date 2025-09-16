7ec59f24b3c21ec79c95da6b5b12cef5c2616f74
VaH31 <92624920+VaH31@users.noreply.github.com>
2025-09-16 12:50:57 +0300

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-<script>
+// текущий год в подвале
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// открыть картинку в лайтбоксе
 function openLightbox(img) {
   document.getElementById("lightbox-img").src = img.src;
   document.getElementById("lightbox").style.display = "flex";
 }
+
+// закрыть лайтбокс
 function closeLightbox() {
   document.getElementById("lightbox").style.display = "none";
 }
-</script>
